6cfeae055ff7860b53920b76722fe62f8c0a85c5
ochirovalexander <ochirovalexander@gmail.com>
2023-08-15 20:44:30 +0600

    test

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <tree-item
+    v-for="(item, index) of currentItems"
+    :item="item"
+    :key="item.id"
+    :color="getColor(item, index)"
+    @update="update($event)"
+  ></tree-item>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import axios from 'axios'
+import TreeItem from './components/TreeItem'
+
+const COLORS = {
+  FIRST: '#BDBDBD',
+  MIDDLE: '#E0E0E0',
+  LAST: '#EEEEEE'
+}
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TreeItem
+  },
+  setup () {
+    let relationships = new Set()
+    const items = ref([])
+    const currentItems = ref([])
+
+    const fetchItems = () => {
+      axios
+        .get('https://64b4c8450efb99d862694609.mockapi.io/tree/items')
+        .then(response => {
+          relationships = new Set(response.data.filter(({ parent_id }) => parent_id !== null).map((item) => item.parent_id))
+          items.value = response.data.map((item) => ({
+            ...item,
+            is_open: false,
+            is_childless: !relationships.has(item.id),
+            level: item.parent_id !== null ? 0 : null
+          }))
+          currentItems.value = items.value.filter(({ parent_id }) => parent_id === null)
+        })
+    }
+
+    fetchItems()
+
+    const getColor = (item, index) => item.parent_id === null ? COLORS.FIRST : index % 2 === 0 ? COLORS.MIDDLE : COLORS.LAST
+
+    const update = ({ id, is_open, level }) => {
+      const parentIndex = currentItems.value.findIndex((item) => item.id === id)
+      currentItems.value[parentIndex].is_open = !currentItems.value[parentIndex].is_open
+      if (is_open) {
+        currentItems.value = currentItems.value.filter(({ parent_id }) => parent_id !== id)
+      } else {
+        const itemsForShow = items.value
+          .filter(item => item.parent_id === id )
+          .map((item) => ({ ...item, level: level + 1 }))
+        currentItems.value.splice(parentIndex + 1, 0, ...itemsForShow)
+      }
+    }
+    return { items, currentItems, getColor, update }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TreeItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="tree-item" >
+    <div :style="treeItemStyle">
+      <div class="tree-item__content" :style="treeItemContentStyle" @click="$emit('update', { ...item })">
+        <img v-if="item.is_childless" alt="has no child" width="15" height="15" src="../assets/dash.svg">
+        <img v-else-if="item.is_open" alt="opened" width="15" height="15" src="../assets/chevron-down.svg">
+        <img v-else alt="closed" width="15" height="15" src="../assets/chevron-right.svg">
+        <div class="tree-item__content__title">{{ title }}</div>        
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue'
+
+export default {
+  name: 'TreeItem',
+  props: ['item', 'color'],
+  setup (props) {
+    const treeItemContentStyle = computed(() => ({
+        transform: `translate(${props.item.level * 15}px)`
+    }))
+    const treeItemStyle = computed(() => ({
+      'background-color': props.color
+    }))
+    return { treeItemContentStyle, treeItemStyle, title: props.item.title }
+  }
+}
+</script>
+
+<style scoped>
+.tree-item {
+  width: 400px;
+  .tree-item__content {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+    .tree-item__content__title {
+      margin-left: 0.5rem;
+      font-size: large;
+    }
+  }
+}
+</style>
